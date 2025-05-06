40fc0f413286dbb637db1e1c5a8e79ccf6230e38
Nak0_x <nak0x@proton.me>
2025-05-06 16:32:39 +0200

    Setup analytics from vercels

--- app.vue
+++ app.vue
@@ -1,6 +1,12 @@
 <template>
+  <Analytics />
   <div>
     <NuxtRouteAnnouncer />
     <NuxtWelcome />
   </div>
 </template>
+
+<script setup>
+  import { Analytics } from '@vercel/analytics/nuxt'
+</script>
+
