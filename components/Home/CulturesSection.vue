5f0c60c068cf84d9b04a666857a714a98724ebc0
Nak0_x <nak0x@proton.me>
2025-05-20 21:46:01 +0200

    Setup the base for the experience section

--- components/Home/CulturesSection.vue
+++ components/Home/CulturesSection.vue
@@ -1,18 +1,36 @@
 <template>
   <div class="cultures-container">
-    <h2 class="title is-1">
+    <h2 class="title is-1 section-title">
       Discover the cultures
     </h2>
-    Door
+    <div class="experiences-container">
+      <section class="experience-section" v-for="exp in experiences" :key="exp.id">
+        <div class="layers">
+          <NuxtImg
+            v-for="i in layers"
+            :key="i"
+            :src="`/images/experience/${exp.id}/layer_${i}.png`"
+            format="avif"
+            placeholder
+            class="layer"
+            aria-hidden
+          />
+        </div>
+      </section>
+      <NuxtImg
+        src="/images/door.png"
+        format="avif"
+        placeholder
+        class="clipping-door"
+        aria-hidden
+      />
+    </div>
   </div>
 </template>
 
-<script>
-export default {
+<script setup>
+  import experiencesDatas from '~/data/experiences.json';
 
-}
+  const layers = 5;
+  const experiences = experiencesDatas.experiences;
 </script>
-
-<style>
-
-</style>
