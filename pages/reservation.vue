5c1891b4be6a70ff760fbe8b02b063b263c21ccf
Nak0_x <nak0x@proton.me>
2025-06-24 22:25:19 +0200

    🚑 HOTFIX: Fix naming

--- pages/reservation.vue
+++ pages/reservation.vue
@@ -32,22 +32,22 @@
 
   const choices = [
     {
-      'title': 'Mexique ancien',
-      'subtitle': 'La terre comme moyen de survie',
+      "title": "MEXIQUE ANCIEN",
+      "subtitle": "La poterie méso-américaine",
       'description': 'Bien plus qu’un simple contenant, la poterie dans les civilisations du Mexique ancien (Mayas, Aztèques, Toltèques…) était un support d’expression, de rituel et de transmission.',
       'longDescription': 'Bien plus qu’un simple contenant, la poterie dans les civilisations du Mexique ancien (Mayas, Aztèques, Toltèques…) était un support d’expression, de rituel et de transmission.',
       'image': '/images/hands-clayering.png',
     },
     {
-      'title': 'Maroc',
-      'subtitle': 'Le pouvoir du cuire',
+      "title": "MAROC ANCIEN",
+      "subtitle": "Le cuir, matière vivante",
       'description': 'Du cœur des tanneries aux mains des artisans, le cuir marocain est bien plus qu’un matériau : c’est un savoir-faire transmis de génération en génération.',
       'longDescription': 'Plus qu’un simple outil d’écriture, la calligraphie en Égypte antique était un art sacré, réservé à une élite formée et respectée.  À travers nos ateliers, les participants découvrent les instruments du scribe, les formes de l’écriture hiéroglyphique, et le rôle politique et spirituel de l’écrit dans cette civilisation fascinante.',
       'image': '/images/grece.jpg',
     },
     {
-      'title': 'Égypte ancienne',
-      'subtitle': 'La calligraphie comme pouvoir',
+      "title": "EGYPTE ANTIQUE",
+      "subtitle": "La calligraphie comme pouvoir",
       'description': 'Plus qu’un simple outil d’écriture, la calligraphie en Égypte antique était un art sacré, réservé à une élite formée et respectée.',
       'longDescription': 'Plus qu’un simple outil d’écriture, la calligraphie en Égypte antique était un art sacré, réservé à une élite formée et respectée.',
       'image': '/images/egypte.jpg',
